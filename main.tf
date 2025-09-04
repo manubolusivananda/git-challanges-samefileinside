01b15c9eede1e66d2fecc7ce210c4fa7aca8bc6c
manubolusivananda <manubolusivananda@gmail.com>
2025-09-04 11:38:53 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ram" {
+resource "aws_instance" "siva" {
 
   ami                         = "ami-0b016c703b95ecbe4" # ✅ Amazon Linux 2 in us-east-1
   instance_type               = "t2.micro"
