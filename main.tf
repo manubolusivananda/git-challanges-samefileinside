5c2630bcad7d01dc85eed2b9df9f266461118e21
manubolusivananda <manubolusivananda@gmail.com>
2025-09-04 11:35:49 +0530

    fgh

---
+++ main.tf
@@ -0,0 +1,11 @@
+resource "aws_instance" "ram" {
+
+  ami                         = "ami-0b016c703b95ecbe4" # ✅ Amazon Linux 2 in us-east-1
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.multi_port.id]
+  associate_public_ip_address = false
+
+  tags = {
+    Name = "my-ec2"
+  }
+} 
